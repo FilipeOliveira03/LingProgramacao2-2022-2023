dbd8abb03e0cc797bf8b7ea2662c38d68c3a2b60
FilipeOliveira03 <filipe.martim.oliveira@gmail.com>
2022-11-06 14:06:13 +0000

    createInitialJungle test

--- src/pt/ulusofona/lp2/deisiJungle/Main.java
+++ src/pt/ulusofona/lp2/deisiJungle/Main.java
@@ -3,14 +3,14 @@ package pt.ulusofona.lp2.deisiJungle;
 public class Main {
     public static  void main(String[] args) {
 
-        String [][]  b = {{"1"}, {"2"}, {"3"},{"4"}};
-        GameManager a = new GameManager(4);
-
-        if(a.createInitialJungle(1, 1, b)) {
-            System.out.println("True");
-        }else{
-            System.out.println("false");
-        }
+//        String [][]  b = {{"1"}, {"2"}, {"3"},{"4"}};
+//        GameManager a = new GameManager(4, 7);
+//
+//        if(a.createInitialJungle(1, 1, b)) {
+//            System.out.println("True");
+//        }else{
+//            System.out.println("false");
+//        }
 
     }
 }
