c5c9fc8cec30aaa9ecddb00f5983841e3b9c0531
FilipeOliveira03 <filipe.martim.oliveira@gmail.com>
2022-11-10 23:04:50 +0000

    moveCurrentPlayer test

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -1,20 +1,21 @@
 package pt.ulusofona.lp2.deisiJungle;
 
 import javax.swing.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 // numeros impares - Henrique
 // numeros pares - Filipe
 
 public class GameManager {
 
-    HashMap <Integer,ArrayList<Player>> tabuleiro = new HashMap<>();
     ArrayList<Player> jogadores = new ArrayList<>();
+    HashMap <Integer,ArrayList<Player>> tabuleiro = new HashMap<>();
 
     int jogadoresMinimos = 2;
     int jogadoresMaximos = 4;
     int meta;
-    //int turno = 0;
 
     public boolean isNumeric(String s) {
         if (s == null || s.equals("")) {
@@ -41,51 +42,78 @@ public class GameManager {
     }
 
     public boolean createInitialJungle(int jungleSize, int initialEnergy, String[][] playersInfo){
+
         int countNrTarzan = 0;
+
         boolean[] verificarEspecie = new boolean[playersInfo.length];
+
         for (int countPlayer1 = 0; countPlayer1 < playersInfo.length; countPlayer1++) {
+
             for (int countPlayer2 = 0; countPlayer2 < playersInfo.length; countPlayer2++) {
+
                 if(countPlayer1 != countPlayer2){ // jogadores iguais
+
                     if(!isNumeric(playersInfo[countPlayer1][0]) || !isNumeric(playersInfo[countPlayer2][0])){
                         return false;
                     }
+
                     int jogador1Int = Integer.parseInt(playersInfo[countPlayer1][0]);
                     if(jogador1Int < 0){ return false; }
+
                     int jogador2Int = Integer.parseInt(playersInfo[countPlayer2][0]);
                     if(jogador2Int < 0){ return false; }
+
                     if(jogador1Int == jogador2Int){ return false; }//verifica se o id é igual
                 }
             }
         }
+
         for (int countPlayer = 0; countPlayer < playersInfo.length; countPlayer++) {
+
             if(playersInfo[countPlayer][1].equals("") || playersInfo[countPlayer][1] == null){ return false; }//nome null ou vazio
+
             if(playersInfo[countPlayer][2].equals("Z")){ countNrTarzan++;}// so pode existir 1 tarzan
+
             for (int countEspecie = 0; countEspecie < getSpecies().length; countEspecie++) {
+
                 if(playersInfo[countPlayer][2].equals(getSpecies()[countEspecie][0])){
                     // verifica se a especie é do getSpecies()
+
                     verificarEspecie[countPlayer] = true;
                 }
             }
         }
+
         if(countNrTarzan > 1 || playersInfo.length * 2 > jungleSize){ return false; } // verifica se só há 1
+
+
         if(playersInfo.length < jogadoresMinimos || playersInfo.length > jogadoresMaximos) { return false; }// verifica o numero de jogadores
+
         for (boolean verificar : verificarEspecie) { //
             if (!verificar) { return false; }
         }
+
         for (int preencherHash = 0; preencherHash < jungleSize; preencherHash++) {
             ArrayList<Player> players = new ArrayList<>();
             tabuleiro.put(preencherHash, players);
         }
+
         meta = jungleSize;
+
         for (String[] info : playersInfo) {
+
             Player jogador = new Player();
+
             jogador.adicionaId(Integer.parseInt(info[0]));
             jogador.adicionaNome(info[1]);
             jogador.adicionaEspecie(info[2]);
             jogador.adicionaEnergiaAtual(initialEnergy);
+
+
             jogadores.add(jogador);
             tabuleiro.get(0).add(jogador);
         }
+
         return true;
     }
 
@@ -114,7 +142,6 @@ public class GameManager {
         if(squareNr > meta || squareNr < 0 || tabuleiro.get(squareNr) == null){
             return null;
         }
-
         int nrJogadorAtual = 1;
         StringBuilder ids = new StringBuilder();
 
@@ -146,7 +173,6 @@ public class GameManager {
         }
 
         if(squareNr == meta){
-            info[0] = "finish.png";
             info[1] = "Meta";
         }else {
             info[1] = "Vazio";
@@ -175,6 +201,9 @@ public class GameManager {
     }
 
     public String[] getCurrentPlayerInfo(){
+
+
+
         return null;
     }
 
@@ -182,54 +211,7 @@ public class GameManager {
         return null;
     }
 
-    public ArrayList<String> ordemParaJogar(){
-
-        return null;
-    }
-
     public boolean moveCurrentPlayer(int nrSquares, boolean bypassValidations){
-/*
-        if(!bypassValidations){
-            if(nrSquares < 1 || nrSquares > 6){
-                return false;
-            }
-        }
-
-        int jogadorJoga = jogadores.get(turno).id;
-        int posJogadorTabuleiro = 0;
-        int posJogadorCasaArray = 0;
-
-        for (int countCasa = 0; countCasa < tabuleiro.size(); countCasa++) {
-
-            ArrayList<Player> array = tabuleiro.get(countCasa);
-
-            for (int countPlayerPos = 0; countPlayerPos < array.size(); countPlayerPos++) {
-
-                Player jogador = array.get(countPlayerPos);
-
-                if(jogadorJoga == jogador.id){
-
-                    posJogadorTabuleiro = countCasa;
-                    posJogadorCasaArray = countPlayerPos;
-
-                }
-            }
-        }
-
-        Player jogador = tabuleiro.get(posJogadorTabuleiro).get(posJogadorCasaArray);
-
-        tabuleiro.get(posJogadorTabuleiro).remove(posJogadorCasaArray);
-
-        posJogadorTabuleiro += nrSquares;
-
-        tabuleiro.get(posJogadorTabuleiro).add(jogador);
-
-        if(turno == jogadores.size() - 1){
-            turno = 0;
-        }else{
-            turno++;
-        }
-*/
         return true;
     }
 
