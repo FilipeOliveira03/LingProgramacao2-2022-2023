b8c172e937d7b5cb7749831cd3719876900a228f
FilipeOliveira03 <filipe.martim.oliveira@gmail.com>
2022-11-10 22:58:40 +0000

    moveCurrentPlayer test

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -14,7 +14,7 @@ public class GameManager {
     int jogadoresMinimos = 2;
     int jogadoresMaximos = 4;
     int meta;
-    int turno = 0;
+    //int turno = 0;
 
     public boolean isNumeric(String s) {
         if (s == null || s.equals("")) {
@@ -41,79 +41,51 @@ public class GameManager {
     }
 
     public boolean createInitialJungle(int jungleSize, int initialEnergy, String[][] playersInfo){
-
         int countNrTarzan = 0;
-
         boolean[] verificarEspecie = new boolean[playersInfo.length];
-
         for (int countPlayer1 = 0; countPlayer1 < playersInfo.length; countPlayer1++) {
-
             for (int countPlayer2 = 0; countPlayer2 < playersInfo.length; countPlayer2++) {
-
                 if(countPlayer1 != countPlayer2){ // jogadores iguais
-
                     if(!isNumeric(playersInfo[countPlayer1][0]) || !isNumeric(playersInfo[countPlayer2][0])){
                         return false;
                     }
-
                     int jogador1Int = Integer.parseInt(playersInfo[countPlayer1][0]);
                     if(jogador1Int < 0){ return false; }
-
                     int jogador2Int = Integer.parseInt(playersInfo[countPlayer2][0]);
                     if(jogador2Int < 0){ return false; }
-
                     if(jogador1Int == jogador2Int){ return false; }//verifica se o id é igual
                 }
             }
         }
-
         for (int countPlayer = 0; countPlayer < playersInfo.length; countPlayer++) {
-
             if(playersInfo[countPlayer][1].equals("") || playersInfo[countPlayer][1] == null){ return false; }//nome null ou vazio
-
             if(playersInfo[countPlayer][2].equals("Z")){ countNrTarzan++;}// so pode existir 1 tarzan
-
             for (int countEspecie = 0; countEspecie < getSpecies().length; countEspecie++) {
-
                 if(playersInfo[countPlayer][2].equals(getSpecies()[countEspecie][0])){
                     // verifica se a especie é do getSpecies()
-
                     verificarEspecie[countPlayer] = true;
                 }
             }
         }
-
         if(countNrTarzan > 1 || playersInfo.length * 2 > jungleSize){ return false; } // verifica se só há 1
-
-
         if(playersInfo.length < jogadoresMinimos || playersInfo.length > jogadoresMaximos) { return false; }// verifica o numero de jogadores
-
         for (boolean verificar : verificarEspecie) { //
             if (!verificar) { return false; }
         }
-
         for (int preencherHash = 0; preencherHash < jungleSize; preencherHash++) {
             ArrayList<Player> players = new ArrayList<>();
             tabuleiro.put(preencherHash, players);
         }
-
         meta = jungleSize;
-
         for (String[] info : playersInfo) {
-
             Player jogador = new Player();
-
             jogador.adicionaId(Integer.parseInt(info[0]));
             jogador.adicionaNome(info[1]);
             jogador.adicionaEspecie(info[2]);
             jogador.adicionaEnergiaAtual(initialEnergy);
-
-
             jogadores.add(jogador);
             tabuleiro.get(0).add(jogador);
         }
-
-       jogadores.sort(Comparator.comparing((Player jogador) -> jogador.id));
         return true;
     }
 
