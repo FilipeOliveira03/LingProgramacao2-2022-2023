53fab4ba2b7f7bef87fc4643b55a4c02561b2aaa
FilipeOliveira03 <filipe.martim.oliveira@gmail.com>
2022-11-11 13:17:07 +0000

    moveCurrentPlayer test

--- src/pt/ulusofona/lp2/deisiJungle/TestGame.java
+++ src/pt/ulusofona/lp2/deisiJungle/TestGame.java
@@ -8,83 +8,83 @@ import static org.junit.Assert.assertEquals;
 
 public class TestGame {
 
-//    @Test
-//    public void testCreateJungle(){
-//        GameManager manager = new GameManager();
-//        String[][] array = {
-//                { "11", null,"E" },
-//                { "2", "Leão","L" },
-//
-//        };
-//
-//        boolean resultadoReal = manager.createInitialJungle(8, 6, array);
-//        boolean resultadoEsperado = true;
-//
-//        assertEquals("testCreateJungle", resultadoEsperado, resultadoReal);
-//    }
-//
-//    @Test
-//    public void testCreateJungleIds(){
-//        GameManager manager = new GameManager();
-//        String[][] array = {
-//                { "1", "crp","Z" },
-//                { "2", "Leão","P" },
-//                { "3", "Leão","Z" },
-//
-//        };
-//
-//        boolean resultadoReal = manager.createInitialJungle(8, 6, array);
-//        boolean resultadoEsperado = false;
-//
-//        assertEquals("testCreateJungleIDS", resultadoEsperado, resultadoReal);
-//    }
-//
-//    @Test
-//    public void testGetSquareInfo(){
-//        GameManager manager = new GameManager();
-//        String[][] array = {
-//                { "1", "crp","Z" },
-//                { "2", "Leão","P" },
-//                { "3", "Leã","P" },
-//                { "4", "Leã","P" },
-//        };
-//
-//        manager.createInitialJungle(8, 6, array);
-//
-//        Player a = manager.tabuleiro.get(0).get(1);
-//
-//        manager.tabuleiro.get(0).remove(1);
-//
-//        manager.tabuleiro.get(3).add(a);
-//
-//        String resultadoReal = manager.getSquareInfo(0)[2];
-//
-//        String resultadoEsperado = "";
-//
-//        assertEquals("testGetSquareInfo", resultadoEsperado, resultadoReal);
-//    }
-//    @Test
-//    public void TestGetPlayerIds(){
-//        String[][] array = {
-//                { "1", "crp","Z" },
-//                { "2", "Leão","P" },
-//        };
-//        GameManager manager = new GameManager();
-//        manager.createInitialJungle(8, 6, array);
-//        Player p1 = new Player();
-//        p1.id= 1;
-//        p1.posicaoAtual= 0;
-//        int [] a = new int[1];
-//        a[0]=p1.id;
-//        int [] resultadoReal = manager.getPlayerIds(0);
-//
-//
-//        System.out.println("epserado"+a[0]+"");
-//        System.out.println("real"+ Arrays.toString(resultadoReal) +"");
-//
-//
-//
-//    }
+    @Test
+    public void testCreateJungle(){
+        GameManager manager = new GameManager();
+        String[][] array = {
+                { "11", null,"E" },
+                { "2", "Leão","L" },
+
+        };
+
+        boolean resultadoReal = manager.createInitialJungle(8, 6, array);
+        boolean resultadoEsperado = true;
+
+        assertEquals("testCreateJungle", resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void testCreateJungleIds(){
+        GameManager manager = new GameManager();
+        String[][] array = {
+                { "1", "crp","Z" },
+                { "2", "Leão","P" },
+                { "3", "Leão","Z" },
+
+        };
+
+        boolean resultadoReal = manager.createInitialJungle(8, 6, array);
+        boolean resultadoEsperado = false;
+
+        assertEquals("testCreateJungleIDS", resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void testGetSquareInfo(){
+        GameManager manager = new GameManager();
+        String[][] array = {
+                { "1", "crp","Z" },
+                { "2", "Leão","P" },
+                { "3", "Leã","P" },
+                { "4", "Leã","P" },
+        };
+
+        manager.createInitialJungle(8, 6, array);
+
+        Player a = manager.tabuleiro.get(0).get(1);
+
+        manager.tabuleiro.get(0).remove(1);
+
+        manager.tabuleiro.get(3).add(a);
+
+        String resultadoReal = manager.getSquareInfo(0)[2];
+
+        String resultadoEsperado = "";
+
+        assertEquals("testGetSquareInfo", resultadoEsperado, resultadoReal);
+    }
+    @Test
+    public void TestGetPlayerIds(){
+        String[][] array = {
+                { "1", "crp","Z" },
+                { "2", "Leão","P" },
+        };
+        GameManager manager = new GameManager();
+        manager.createInitialJungle(8, 6, array);
+        Player p1 = new Player();
+        p1.id= 1;
+        p1.posicaoAtual= 0;
+        int [] a = new int[1];
+        a[0]=p1.id;
+        int [] resultadoReal = manager.getPlayerIds(0);
+
+
+        System.out.println("epserado"+a[0]+"");
+        System.out.println("real"+ Arrays.toString(resultadoReal) +"");
+
+
+
+    }
 
 
 
