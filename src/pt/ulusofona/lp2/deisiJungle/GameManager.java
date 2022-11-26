243870b5e7d8117965cd7733f916ed898cf8a45a
FilipeOliveira03 <filipe.martim.oliveira@gmail.com>
2022-11-26 18:39:56 +0000

    dp funcionar

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 
 public class GameManager {
