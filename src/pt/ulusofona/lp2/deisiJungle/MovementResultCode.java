c4912eae50f397317732f2c46afc345b66c1bdb0
FilipeOliveira03 <filipe.martim.oliveira@gmail.com>
2022-11-26 18:31:04 +0000

    modificação ficheiros

--- src/pt/ulusofona/lp2/deisiJungle/MovementResultCode.java
+++ src/pt/ulusofona/lp2/deisiJungle/MovementResultCode.java
@@ -1,4 +1,13 @@
 package pt.ulusofona.lp2.deisiJungle;
 
 public class MovementResultCode {
+
+    String INVALID_MOVEMENT;
+    String NO_ENERGY;
+    String VALID_MOVEMENT;
+    String CAUGHT_FOOD;
+
+    public static String code(){
+        return "";
+    }
 }
