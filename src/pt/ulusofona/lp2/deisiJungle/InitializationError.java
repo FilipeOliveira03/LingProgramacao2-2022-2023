a16c2595f4e0ebaf73f870735e1d7c84e2c9e5ce
FilipeOliveira03 <filipe.martim.oliveira@gmail.com>
2022-12-07 09:21:51 +0000

    dp funcionar

--- src/pt/ulusofona/lp2/deisiJungle/InitializationError.java
+++ src/pt/ulusofona/lp2/deisiJungle/InitializationError.java
@@ -2,41 +2,15 @@ package pt.ulusofona.lp2.deisiJungle;
 
 import static pt.ulusofona.lp2.deisiJungle.InitializationErrorCode.*;
 
-public record InitializationError(InitializationErrorCode code){
+public class InitializationError{
 
     public String getMessage(Boolean inicializou){
 
         String mensagem = "";
-        if(!inicializou){
-            if(code == INVALID_IDS){
-
-                mensagem = "Os IDS são inválidos";
-
-            }else if(code == INVALID_NAME){
-
-                mensagem = "O nome é inválido";
-
-            }else if(code == INVALID_SPECIE){
-
-                mensagem = "A espécie é inválida";
-
-            }else if(code == INVALID_FOOD){
-
-                mensagem = "A comida é inválida";
-
-            }else if(code == INVALID_POSITION_FOOD){
-
-                mensagem = "A posição da comida é inválida";
-
-            }else if(code == INVALID_NUM_PLAYERS){
-
-                mensagem = "Numero de jogadores inválido";
-
-            }else if(code == INVALID_MAP_CONFIGURATION){
-
-                mensagem = "Não existem, pelo menos, duas posições por cada jogador";
-
-            }
+        if(inicializou){
+            mensagem = "yes";
+        }else{
+            mensagem = "no";
         }
 
 
