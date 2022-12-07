c188a7246106a2b5b8f114ae34bfc31d30884949
FilipeOliveira03 <filipe.martim.oliveira@gmail.com>
2022-12-07 09:20:13 +0000

    dp funcionar

--- src/pt/ulusofona/lp2/deisiJungle/InitializationError.java
+++ src/pt/ulusofona/lp2/deisiJungle/InitializationError.java
@@ -4,39 +4,41 @@ import static pt.ulusofona.lp2.deisiJungle.InitializationErrorCode.*;
 
 public record InitializationError(InitializationErrorCode code){
 
-    public String getMessage(){
+    public String getMessage(Boolean inicializou){
 
         String mensagem = "";
+        if(!inicializou){
+            if(code == INVALID_IDS){
 
-        if(code == INVALID_IDS){
+                mensagem = "Os IDS são inválidos";
 
-            mensagem = "Os IDS são inválidos";
+            }else if(code == INVALID_NAME){
 
-        }else if(code == INVALID_NAME){
+                mensagem = "O nome é inválido";
 
-            mensagem = "O nome é inválido";
+            }else if(code == INVALID_SPECIE){
 
-        }else if(code == INVALID_SPECIE){
+                mensagem = "A espécie é inválida";
 
-            mensagem = "A espécie é inválida";
+            }else if(code == INVALID_FOOD){
 
-        }else if(code == INVALID_FOOD){
+                mensagem = "A comida é inválida";
 
-            mensagem = "A comida é inválida";
+            }else if(code == INVALID_POSITION_FOOD){
 
-        }else if(code == INVALID_POSITION_FOOD){
+                mensagem = "A posição da comida é inválida";
 
-            mensagem = "A posição da comida é inválida";
+            }else if(code == INVALID_NUM_PLAYERS){
 
-        }else if(code == INVALID_NUM_PLAYERS){
+                mensagem = "Numero de jogadores inválido";
 
-            mensagem = "Numero de jogadores inválido";
+            }else if(code == INVALID_MAP_CONFIGURATION){
 
-        }else if(code == INVALID_MAP_CONFIGURATION){
+                mensagem = "Não existem, pelo menos, duas posições por cada jogador";
 
-            mensagem = "Não existem, pelo menos, duas posições por cada jogador";
-
+            }
         }
+
 
         return mensagem;
     }
