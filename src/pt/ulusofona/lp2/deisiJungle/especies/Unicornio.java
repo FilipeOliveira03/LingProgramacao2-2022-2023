e61ce713339fbc6ee16d15fa3d57632fca719bfe
Henrique Franco <henriquempgf@gmail.com>
2023-01-28 13:03:32 +0000

    post move

---
+++ src/pt/ulusofona/lp2/deisiJungle/especies/Unicornio.java
@@ -0,0 +1,16 @@
+package pt.ulusofona.lp2.deisiJungle.especies;
+
+
+public class Unicornio extends Especie {
+
+    public Unicornio(){
+        this.nomeSigla = "Z";
+        this.energiaAtual = 70;
+        this.consumoEnergetico = 2;
+        this.ganhoEnerDescanso = 20;
+        this.velocidade = "1..6";
+        this.tipoAlimentacao = "Omnívoro";
+        this.nomeEspecie = "Tarzan";
+    }
+
+}
