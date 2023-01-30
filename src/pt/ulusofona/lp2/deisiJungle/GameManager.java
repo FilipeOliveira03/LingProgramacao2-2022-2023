e3d1af1d9f2bca2dd74934c6d957b8c99b07c268
Henrique Franco <henriquempgf@gmail.com>
2023-01-30 12:56:59 +0000

    post move

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -547,9 +547,9 @@ public class GameManager {
             }
         }
         if(countJogCasaMeio==2&&countJogAFrenteCasaMeio>0){
-
+            String[] infojogadorvencedor = new String[4];
  if (jogadoresDoMeio.get(0).getEspecie().getEnergiaAtual()>jogadoresDoMeio.get(1).getEspecie().getEnergiaAtual()){
-     String[] infojogadorvencedor = new String[4];
+
 
      infojogadorvencedor[0] = String.valueOf(jogadoresDoMeio.get(0).getID());
      infojogadorvencedor[1] = jogadoresDoMeio.get(0).getNome();
@@ -558,7 +558,7 @@ public class GameManager {
      mudarTurno();
      return infojogadorvencedor;
             }else{
-     String[] infojogadorvencedor = new String[4];
+
 
      infojogadorvencedor[0] = String.valueOf(jogadoresDoMeio.get(1).getID());
      infojogadorvencedor[1] = jogadoresDoMeio.get(1).getNome();
